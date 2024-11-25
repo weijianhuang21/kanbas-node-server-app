9bab75262780636182f8f2c114b306420cd12a86
Jacob <learningmachine999@gmail.com>
2024-11-24 16:03:17 -0800

    update

--- App.js
+++ App.js
@@ -11,28 +11,31 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 
 const app = express();
-app.use(cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-}));
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+   );
 
 
 
-
-const sessionOptions = {
+   const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
     resave: false,
     saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
+  };
+  
+  if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
     };
-}
-app.use(session(sessionOptions));
+  }
+  app.use(session(sessionOptions));
+  
 
 
 app.use(express.json());
